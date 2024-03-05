ec55e8898b7032c0ba4cbe3da314db3485babb30
davidacs10 <davidacs10.08@gmail.com>
2024-03-05 09:00:49 -0400

    CRUD Finished

--- routers/user.py
+++ routers/user.py
@@ -1,13 +1,22 @@
 from fastapi import APIRouter, HTTPException, status
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
-class User(BaseModel):
+class UserBase(BaseModel):
     id: int
     username: str
-    password: str
+    email: EmailStr
 
-users_list = [User(id=1, username="david01", password="123456"),
-              User(id=2, username="david02", password="654321"),]
+class UserIn(UserBase):
+    password: str = ...
+
+class UserOut(UserBase):
+    pass
+
+class UserInDB(UserBase):
+    hashed_password: str
+
+users_list = [UserIn(id=1, username="david01", password="123456", email="david11@dev.com"),
+              UserIn(id=2, username="david02", password="654321", email="david10@dev.com"),]
 
 router = APIRouter(prefix="/users")
 
@@ -19,15 +28,39 @@ async def user():
 async def user_by_id(id: int):
     return search_id(id)
 
-@router.post("/", response_model=User, status_code=201)
-async def create_user(user: User):
-    if type(search_id(user.id)) == User:
+@router.post("/", response_model=UserBase, status_code=201)
+async def create_user(user: UserIn):
+    if any(u.id == user.id for u in users_list):
             raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                                 detail="Ya existe un usuario con ese ID")
     
     users_list.append(user)
     return user
 
+@router.put("/")
+async def update_user(user: UserIn):
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            return user
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="ID Not found",
+                        headers={"404":"Not found"})
+        
+@router.delete("/{id}")
+async def delete_user(id: int):
+    
+    for index, user in enumerate(users_list):
+        if user.id == id:
+            del users_list[index]
+            raise HTTPException(status_code=status.HTTP_202_ACCEPTED,
+                                detail="User successfully deleted",
+                                headers={"202":"User deleted"})
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="User not found",
+                        headers={"404":"User not found"})
 
 # Search
 def search_id(id: int):
