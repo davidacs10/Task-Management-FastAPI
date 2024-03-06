fb4ba38a54585feaae1edd0bbed7b3a408a05a82
davidacs10 <davidacs10.08@gmail.com>
2024-03-06 11:10:57 -0400

    task API

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from routers import user
+from routers import user, task
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(task.router)
 
 # uvicorn main:app --reload
 
